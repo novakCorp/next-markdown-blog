5719ecaf6a5031152027f7df669c6f529e0bd569
nova <nova.corps1010@outlook.com>
2022-03-01 16:40:02 -0400

    displaying posts

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,51 @@
+import fs from 'fs';
+import path from 'path';
 import Head from 'next/head'
+import matter from 'gray-matter';
+import Post from '../components/Post';
 
-
-export default function Home() {
+export default function Home({posts}) {
   return (
     <div>
       <Head>
         <title>Dev Blog</title>
       </Head>
-      <h2>Hello</h2>
+      
+      <div className="posts">
+        {posts.map((post, index) => (
+          <Post post={post} />
+          ))}
+      </div>
+
     </div>
   )
+} 
+
+// fetch data at build time 
+export async function getStaticProps() {
+
+  const files = fs.readdirSync(path.join('posts'));
+  // get slug and frontmatter from posts
+  const posts = files.map(filename => {
+
+     // Create slug
+    const slug = filename.replace('.md', '');
+
+    // Get frontmatter which basically means the data
+    const markdownWithMeta = fs.readFileSync(path.join('posts', filename), 'utf-8');
+    const {data:frontmatter} = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter
+    }
+  })
+
+  console.log(posts);
+
+  return {
+    props: {
+      posts,
+    }
+  }
 }
